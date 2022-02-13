79ecbc07bb4ec72eb00a0b835353d3c0987ca683
qiu-x <alex@alexslomka.xyz>
2022-02-13 17:07:48 +0100

    scrollbar fix

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function moveSearch() {
 	search.style.transform = 'translateY(-100%)';
 	vids = document.getElementsByClassName("video")
 	for (let i = 0; i < vids.length; i++) {
+		vids[i].style.display = "inline-block";
 		sleep(i*500).then(() => {
 			vids[i].classList.add("showed");
 		})
