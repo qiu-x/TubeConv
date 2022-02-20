25f676832efb4622c4c3f9f5be4eb5d2e9af53d2
GNUSheep <pentet221@gmail.com>
2022-02-20 12:15:48 +0000

    Change way of parsing video from youtube

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module TubeConv
 go 1.15
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/gorilla/mux v1.8.0
-	google.golang.org/api v0.69.0
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 )
--- request.go
+++ request.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
-	"google.golang.org/api/googleapi/transport"
-	"google.golang.org/api/youtube/v3"
+	"github.com/PuerkitoBio/goquery"
+	//"google.golang.org/api/googleapi/transport"
+	//"google.golang.org/api/youtube/v3"
 )
 
-const developerKey = "AIzaSyAJ46IxN6B6u3BfWODc0AZFP3A1fdtBH-I"
+const developerKey = ""
 
 func err_handle(err error) {
 	log.Println(err)
@@ -55,28 +57,34 @@ func query_request(w http.ResponseWriter, body []byte) {
 	json.Unmarshal(body, &query_req)
 	log.Println(query_req)
 
-	client := &http.Client{
-		Transport: &transport.APIKey{Key: developerKey},
+	text := strings.ReplaceAll(query_req.Text, " ", "+")
+	parse, err := http.Get("https://www.youtube.com/results?search_query=" + text)
+	if err != nil {
+		err_handle(err)
 	}
-	service, err := youtube.New(client)
+	defer parse.Body.Close()
+
+	html, err := goquery.NewDocumentFromReader(parse.Body)
 	if err != nil {
 		err_handle(err)
 	}
 
-	// Youtube API use
-	var look_by []string
-	look_by = append(look_by, "snippet")
+	index := strings.Index(html.Text(), "ytInitialData")
+	ytInitialData := strings.Split(html.Text()[index:], "if (")[0]
+	ytInitialData = strings.Split(ytInitialData, "ytInitialData =")[1]
+	ytInitialData_byte := []byte(ytInitialData)[:len(ytInitialData) - 1]
+	ytInitialData = string(ytInitialData_byte)
 
-	call := service.Search.List(look_by).Q(query_req.Text).MaxResults(25)
-	result, err := call.Do()
+	var ytInitialDataJSON map[string]interface{}
+	err = json.Unmarshal([]byte(ytInitialData), &ytInitialDataJSON)
 	if err != nil {
 		err_handle(err)
 	}
-
-	// Create and send respond
-	w.Header().Set("Content-Type", "application/json")
-	//var videos [5][4]string
-
+	contents := ytInitialDataJSON["contents"].(map[string]interface{})["twoColumnSearchResultsRenderer"].
+	(map[string]interface{})["primaryContents"].(map[string]interface{})["sectionListRenderer"].
+	(map[string]interface{})["contents"].([]interface{})[0].(map[string]interface{})["itemSectionRenderer"].
+	(map[string]interface{})["contents"].([]interface{})
+	
 	type video struct {
 		Title     string `json:"title"`
 		Author    string `json:"author"`
@@ -85,13 +93,20 @@ func query_request(w http.ResponseWriter, body []byte) {
 	}
 	var vids []video
 
-	for _, item := range result.Items {
-		switch item.Id.Kind {
-		case "youtube#video":
-			vid := video{item.Snippet.Title, item.Snippet.ChannelTitle,
-				"https://youtube.com/watch?v=" + item.Id.VideoId, item.Snippet.Thumbnails.High.Url}
-			vids = append(vids, vid)
+	for _, v := range contents {
+		if v.(map[string]interface{})["videoRenderer"] == nil {
+			continue
 		}
+		vid := video{v.(map[string]interface{})["videoRenderer"].(map[string]interface{})["title"].
+		(map[string]interface{})["runs"].([]interface{})[0].(map[string]interface{})["text"].(string), 
+		v.(map[string]interface{})["videoRenderer"].(map[string]interface{})["longBylineText"].
+		(map[string]interface{})["runs"].([]interface{})[0].(map[string]interface{})["text"].(string),
+		"https://www.youtube.com/watch?v=" + v.(map[string]interface{})["videoRenderer"].
+		(map[string]interface{})["videoId"].(string), 
+		v.(map[string]interface{})["videoRenderer"].(map[string]interface{})["thumbnail"].
+		(map[string]interface{})["thumbnails"].([]interface{})[0].(map[string]interface{})["url"].(string)}
+
+		vids = append(vids, vid)
 	}
 	videos := struct {
 		Videos []video `json:"videos"`
@@ -102,3 +117,59 @@ func query_request(w http.ResponseWriter, body []byte) {
 	}
 	w.Write(videos_json)
 }
+
+//func query_request_old(w http.ResponseWriter, body []byte) {
+//	query_req := struct {
+//		Request string
+//		Text    string
+//	}{}
+//	json.Unmarshal(body, &query_req)
+//	log.Println(query_req)
+//
+//	client := &http.Client{
+//		Transport: &transport.APIKey{Key: developerKey},
+//	}
+//	service, err := youtube.New(client)
+//	if err != nil {
+//		err_handle(err)
+//	}
+//
+//	// Youtube API use
+//	var look_by []string
+//	look_by = append(look_by, "snippet")
+//
+//	call := service.Search.List(look_by).Q(query_req.Text).MaxResults(25)
+//	result, err := call.Do()
+//	if err != nil {
+//		err_handle(err)
+//	}
+//
+//	// Create and send respond
+//	w.Header().Set("Content-Type", "application/json")
+//	//var videos [5][4]string
+//
+//	type video struct {
+//		Title     string `json:"title"`
+//		Author    string `json:"author"`
+//		Link      string `json:"link"`
+//		Thumbnail string `json:"thumbnail"`
+//	}
+//	var vids []video
+//
+//	for _, item := range result.Items {
+//		switch item.Id.Kind {
+//		case "youtube#video":
+//			vid := video{item.Snippet.Title, item.Snippet.ChannelTitle,
+//				"https://youtube.com/watch?v=" + item.Id.VideoId, item.Snippet.Thumbnails.High.Url}
+//			vids = append(vids, vid)
+//		}
+//	}
+//	videos := struct {
+//		Videos []video `json:"videos"`
+//	}{Videos: vids}
+//	videos_json, err := json.Marshal(videos)
+//	if err != nil {
+//		err_handle(err)
+//	}
+//	w.Write(videos_json)
+//}
